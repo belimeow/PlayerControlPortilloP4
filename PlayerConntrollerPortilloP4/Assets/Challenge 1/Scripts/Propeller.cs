6bd49a03ab4c0cac6ca9cd643303ea3ba83378a7
belimeow <711592@student.dusd.net>
2023-02-27 11:21:13 -0800

    challangeone
    I did my challenge one and even did the bonus propeller

---
+++ PlayerConntrollerPortilloP4/Assets/Challenge 1/Scripts/Propeller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propeller : MonoBehaviour
+{
+    private float propeller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.forward * propeller * 40);
+    }
+}
